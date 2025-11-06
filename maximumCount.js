65bb504e5be7b97c0426cf5653cfa2d5fc7abc45
Hamid997 <hamid.birouk0597@gmail.com>
2025-11-06 22:29:14 +0100

    day 51

---
+++ maximumCount.js
@@ -0,0 +1,77 @@
+/*
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+Note that 0 is neither positive nor negative.
+
+
+
+Example 1:
+Input: nums = [-2,-1,-1,1,2,3]
+Output: 3
+Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+Example 2:
+Input: nums = [-3,-2,-1,0,0,1,2]
+Output: 3
+Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+Example 3:
+Input: nums = [5,20,66,1314]
+Output: 4
+Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.
+ 
+
+Constraints:
+1 <= nums.length <= 2000
+-2000 <= nums[i] <= 2000
+nums is sorted in a non-decreasing order.
+ 
+
+Follow up: Can you solve the problem in O(log(n)) time complexity?
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+**/
+
+var maximumCount = function(nums) {
+    let neg = 0;
+    let pos = 0;
+    
+    // Binary search for counting negatives
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] < 0) {
+            neg = mid + 1;  // count negatives
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    // Binary search for counting positives
+    // RESET left & right for second search
+    left = 0;
+    right = nums.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > 0) {
+            pos = nums.length - mid;  // count positives
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    // Compare and return max
+    return Math.max(neg, pos);
+};
+
+
+console.log(maximumCount([-2,-1,-1,1,2,3]));    // 3
+console.log(maximumCount([-3,-2,-1,0,0,1,2]));  // 3
+console.log(maximumCount([5,20,66,1314]));      // 4
